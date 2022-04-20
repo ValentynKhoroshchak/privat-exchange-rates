3724ec2101b473250240c326091caae98aaac3a6
Valentyn Khoroshchak <valentyn.khoroshchak@jetsoftpro.com>
2022-04-21 02:10:01 +0300

    Setup the base structure of library

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+DESCRIPTION = ('A small library for getting information on cash '
+               'exchange rates of PrivatBank and the NBU on the selected date')
+
+setup(
+    name="privat_exchange_rates",
+    version=VERSION,
+    author="Valentyn Khoroshchak",
+    author_email="valentin.khoroshchak@gmail.com",
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=find_packages(),
+    install_requires=['requests'],
+
+    keywords=['python', 'exchange rate', 'privatbank'],
+    classifiers=[
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
